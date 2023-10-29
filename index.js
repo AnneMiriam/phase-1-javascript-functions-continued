e8e8c4a74c56d34fd7ed64b9231d78c06275471d
Anne Hastings <anne.miriam@gmail.com>
2023-10-29 16:38:55 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity){
+    if (activity){
+        return `This Saturday, I want to ${activity}!`;
+    } else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+};
+
+const mondayWork = function(activity = 'go to the office'){
+    if (activity){
+        return `This Monday, I will ${activity}.`;
+    }
+};
+
+function wrapAdjective(wrap = '*'){
+    return function(msg = 'special'){
+        return `You are ${wrap}${msg}${wrap}!`;
+    }
+}
